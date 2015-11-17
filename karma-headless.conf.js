3e3a7880b015cb9694f8ddf80862b5881343a98c
Chris Atkins <chrisatkins55@gmail.com>
2015-11-16 21:25:54 -0500

    headless karma config now extends 'normal' karma config, so changes should mostly only be needed in one place

--- karma-headless.conf.js
+++ karma-headless.conf.js
@@ -1,34 +1,18 @@
 module.exports = function(config){
+  var baseProjectConfig = require('./karma.conf.js');
+  
+  baseProjectConfig(config)
+  
   config.set({
 
-    basePath : './',
-
-    files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
-      'app/view*/**/*.js'
-    ],
-
-    autoWatch : true,
-
-    frameworks: ['jasmine'],
+    autoWatch : false,
 
     browsers : ['PhantomJS'],
 
     plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
             'karma-phantomjs-launcher',
             'karma-jasmine',
             'karma-junit-reporter'
             ],
-
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
-
   });
 };
