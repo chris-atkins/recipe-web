ad3e4ec50cdb279555ef5d6ae6f61eff7367ff6d
Chris Atkins <chrisatkins55@gmail.com>
2016-04-08 19:16:43 -0400

    search page now searches! eventually should combine the recipe list section from search and browse-all pages

--- app/search/search-recipes.js
+++ app/search/search-recipes.js
@@ -9,6 +9,18 @@ angular.module('recipe.searchRecipes', ['ngRoute'])
 	});
 }])
 
-.controller('SearchRecipesCtrl', function() {
+.controller('SearchRecipesCtrl', function($scope, $http) {
 	
+	$scope.recipeList = [];
+	$scope.searchString = '';
+	
+	$scope.searchRecipes = function() {
+		$http.get('/api/recipe?searchString=' + $scope.searchString)
+			.success(function(data) {
+				$scope.recipeList = data;
+			})
+			.error(function(error) {
+				console.log('Error getting recipes:', error);
+			});
+	}
 });
