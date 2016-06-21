eb4b31d66e708872effa930f08ca49bb9374429f
Chris Atkins <chrisatkins55@gmail.com>
2016-06-20 22:02:08 -0400

    refactoring save recipe with error

--- app/new-recipe/new-recipe.js
+++ app/new-recipe/new-recipe.js
@@ -2,39 +2,49 @@
 
 angular.module('recipe.newRecipe', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/new-recipe', {
-		templateUrl: 'new-recipe/new-recipe.html',
-		controller: 'NewRecipeCtrl'
-	});
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/new-recipe', {
+            templateUrl: 'new-recipe/new-recipe.html',
+            controller: 'NewRecipeCtrl'
+        });
+    }])
 
-.controller('NewRecipeCtrl', function($scope, $http, userService) {
-	
-	$scope.newRecipeName = '';
-	$scope.newRecipeContent = '';
-	$scope.attemptedToSaveWithNoLogin = false;
-	
-	$scope.saveRecipe = function() {
-		var recipeToSave = {
-			recipeName: $scope.newRecipeName, 
-			recipeContent: $scope.newRecipeContent
-		};
-		
-		$http.post('/api/recipe', recipeToSave)
-			.success(function(data) {
-				//don't do anything
-			})
-			.error(function(error) {
-				console.log('failure saving recipe:', error);
-			});
-	};
+    .controller('NewRecipeCtrl', function ($scope, $http, $location, userService) {
 
-	$scope.isLoggedIn = function() {
-		return userService.isLoggedIn();
-	};
+        $scope.newRecipeName = '';
+        $scope.newRecipeContent = '';
+        var attemptedToSaveWithNoLogin = false;
 
-	$scope.saveClickedWithNoLogin = function() {
-		$scope.attemptedToSaveWithNoLogin = true;
-	};
-});
+        $scope.saveRecipeAndNavigate = function () {
+            if (isNotLoggedIn()) {
+                attemptedToSaveWithNoLogin = true;
+                return;
+            } else {
+                saveRecipe();
+                $location.path('#/home');
+            }
+        };
+
+        $scope.shouldShowErrorMessage = function () {
+            return attemptedToSaveWithNoLogin;
+        };
+
+        function isNotLoggedIn() {
+            return !userService.isLoggedIn();
+        }
+
+        function saveRecipe() {
+            var recipeToSave = {
+                recipeName: $scope.newRecipeName,
+                recipeContent: $scope.newRecipeContent
+            };
+
+            $http.post('/api/recipe', recipeToSave)
+                .success(function (data) {
+                    //don't do anything
+                })
+                .error(function (error) {
+                    console.log('failure saving recipe:', error);
+                });
+        }
+    });
