235db4c933b48d5f18c8735634a0c9721cf8a476
Chris Atkins <chrisatkins55@gmail.com>
2017-04-08 17:04:40 -0400

    first good test for recipe image uploads

--- e2e-tests/endpoint-tests/recipe-image-endpoint-spec.js
+++ e2e-tests/endpoint-tests/recipe-image-endpoint-spec.js
@@ -2,8 +2,9 @@
 var dataUtils = require('./../utils/data-utils');
 var config = browser.params;
 var rs = require('request-promise');
+var fs = require('fs');
 
-describe('the Recipe Image endpoints', function() {
+describe('the Recipe Image endpoints', function () {
 
 	var recipeId;
 	var userId;
@@ -13,40 +14,52 @@ describe('the Recipe Image endpoints', function() {
 		recipeContent: 'First Recipe Content'
 	};
 
-	beforeAll(function(done) {
+	beforeAll(function (done) {
 		var email = dataUtils.randomEmail();
 		var user = {userName: 'ohai', userEmail: email};
 
 		dataUtils.postUser(user)
-		.then(function(user) {
+		.then(function (user) {
 			userId = user.userId;
 			return dataUtils.addRecipe(recipe1, user.userId);
 		})
-		.then(function(recipe) {
+		.then(function (recipe) {
 			recipeId = recipe.recipeId;
 		})
 		.then(done, done.fail);
 	});
 
-	afterAll(function(done) {
+	afterAll(function (done) {
 		dataUtils.cleanupData(done);
 	});
 
 	function performRecipeImagePOST(options) {
-		var postOptions = {
-			uri : config.apiBaseUrl + '/recipe/' + recipeId + '/image',
-			headers : {
-				'Content-Type' : 'multipart/form-data; boundary=----WebKitFormBoundary6kNHU5Tc5TWXmnHV',
-				'Content-Length' : 1234,
-				'RequestingUser' : userId,
-				'Accept': 'application/json'
-			},
-			body: '------WebKitFormBoundary6kNHU5Tc5TWXmnHV\n' +
-			'Content-Disposition: form-data; name="file"; filename="/Users/chrisatkins/git/recipe-web/e2e-tests/endpoint-tests/pexels-photo-48726.jpeg"\n' +
-			'Content-Type: image/png\n\n' +
-			'------WebKitFormBoundary6kNHU5Tc5TWXmnHV--',
+		// var postOptions = {
+		// 	uri : config.apiBaseUrl + '/recipe/' + recipeId + '/image',
+		// 	headers : {
+		// 		'Content-Type' : 'multipart/form-data; boundary=----WebKitFormBoundary6kNHU5Tc5TWXmnHV',
+		// 		'Content-Length' : 1234,
+		// 		'RequestingUser' : userId,
+		// 		'Accept': 'application/json'
+		// 	},
+		// 	body: '------WebKitFormBoundary6kNHU5Tc5TWXmnHV\n' +
+		// 	'Content-Disposition: form-data; name="file"; filename="/Users/chrisatkins/git/recipe-web/e2e-tests/endpoint-tests/pexels-photo-48726.jpeg"\n' +
+		// 	'Content-Type: image/png\n\n' +
+		// 	'------WebKitFormBoundary6kNHU5Tc5TWXmnHV--',
+		//
+		// 	simple: false //https://github.com/request/request-promise
+		// };
+		var stats = fs.statSync('/Users/chrisatkins/git/recipe-web/e2e-tests/endpoint-tests/pexels-photo-48726.jpeg');
+		var fileSizeInBytes = stats.size;
 
-			simple: false //https://github.com/request/request-promise
+		var postOptions = {
+			uri: config.apiBaseUrl + '/recipe/' + recipeId + '/image',
+			headers: {
+				'RequestingUser': userId
+			},
+			formData: {
+				'file': fs.createReadStream('/Users/chrisatkins/git/recipe-web/e2e-tests/endpoint-tests/pexels-photo-48726.jpeg')
+			}
 		};
 
 		if (options && options.responseType && options.responseType === 'full') {
@@ -59,14 +72,15 @@ describe('the Recipe Image endpoints', function() {
 			postOptions.headers.RequestingUser = options.authUserId;
 		}
 
-		return rs.post(postOptions).then(function(response){
+		return rs.post(postOptions).then(function (response) {
 			return response;
 		});
 	}
 
 	function performGETImage(url) {
 		var getOptions = {
-			uri : url,
+			uri: url,
+			resolveWithFullResponse: true,
 			simple: false
 		};
 		return rs.get(getOptions);
@@ -95,16 +109,18 @@ describe('the Recipe Image endpoints', function() {
 		// return rs.del(deleteOptions);
 	}
 
-	fit('can POST a recipe image to a recipe and get a url where the image can be downloaded', function(done) {
+	it('can POST a recipe image to a recipe and get a url where the image can be downloaded', function (done) {
 
 		performRecipeImagePOST({responseType: 'full'})
-		.then(function(response) {
+		.then(function (response) {
+			var body = JSON.parse(response.body);
+
 			expect(response.statusCode).toBe(200);
-			expect(response.body.id).toBeTruthy();
-			expect(response.body.url).toBeTruthy();
-			return performGETImage(getImage(url));
+			expect(body.imageId).toBeTruthy();
+			expect(body.imageUrl).toBeTruthy();
+			return performGETImage(body.imageUrl);
 		})
-		.then(function(response) {
+		.then(function (response) {
 			expect(response.statusCode).toBe(200);
 		})
 		.then(done, done.fail);
@@ -118,7 +134,7 @@ describe('the Recipe Image endpoints', function() {
 		// .then(done, done.fail);
 	});
 
-	it('once posted, an image will have a link that is part of the recipe', function(done) {
+	it('once posted, an image will have a link that is part of the recipe', function (done) {
 		// performRecipeBookPOSTRecipe({recipeId: listOfRecipeIds[0]})
 		// .then(function() {
 		// 	return performRecipeBookPOSTRecipe({recipeId: listOfRecipeIds[1]});
@@ -137,7 +153,7 @@ describe('the Recipe Image endpoints', function() {
 		done();
 	});
 
-	it('the recipe image can be DELETED', function(done) {
+	it('the recipe image can be DELETED', function (done) {
 		// var sizeWithAllIds;
 		// var recipeIdToDelete = listOfRecipeIds[2];
 		//
@@ -166,9 +182,9 @@ describe('the Recipe Image endpoints', function() {
 		done();
 	});
 
-	describe('authorization', function() {
+	describe('authorization', function () {
 
-		it('attempting to POST an image when you are not the owner of the recipe will cause a 401 and will not add the image to the recipe', function(done) {
+		it('attempting to POST an image when you are not the owner of the recipe will cause a 401 and will not add the image to the recipe', function (done) {
 
 			// performRecipeBookPOSTRecipe({recipeId: listOfRecipeIds[2]}, {authUserId: '577d1a8e3dcc7d0c76cb72d0', responseType: 'full'})
 			// .then(function(response) {
@@ -184,7 +200,7 @@ describe('the Recipe Image endpoints', function() {
 			done();
 		});
 
-		it('attempting to POST an image with no user will return 401 and will not add the image to the recipe', function(done) {
+		it('attempting to POST an image with no user will return 401 and will not add the image to the recipe', function (done) {
 
 			// performRecipeBookPOSTRecipe({recipeId: listOfRecipeIds[2]}, {authUserId: 'none', responseType: 'full'})
 			// .then(function(response) {
@@ -200,7 +216,7 @@ describe('the Recipe Image endpoints', function() {
 			done();
 		});
 
-		it('attempting to DELETE an image, if the user does not own the recipe, will return 401 and will not delete the image', function(done) {
+		it('attempting to DELETE an image, if the user does not own the recipe, will return 401 and will not delete the image', function (done) {
 			// performRecipeBookPOSTRecipe({recipeId: listOfRecipeIds[0]})
 			// .then(function() {
 			// 	return performRecipeBookDELETERecipe(userId, listOfRecipeIds[0], {authUserId: '577d1a8e3dcc7d0c76cb72d0', responseType: 'full'});
@@ -218,7 +234,7 @@ describe('the Recipe Image endpoints', function() {
 			done();
 		});
 
-		it('attempting to DELETE an image, if no user is logged in, will return 401 and will not delete the image', function(done) {
+		it('attempting to DELETE an image, if no user is logged in, will return 401 and will not delete the image', function (done) {
 			// performRecipeBookPOSTRecipe({recipeId: listOfRecipeIds[0]})
 			// .then(function() {
 			// 	return performRecipeBookDELETERecipe(userId, listOfRecipeIds[0], {authUserId: 'none', responseType: 'full'});
@@ -237,9 +253,9 @@ describe('the Recipe Image endpoints', function() {
 		});
 	});
 
-	describe('edge cases', function() {
+	describe('edge cases', function () {
 
-		it('attempting to DELETE an image that does not exist in a recipe, will return 404', function(done) {
+		it('attempting to DELETE an image that does not exist in a recipe, will return 404', function (done) {
 			// var unknownRecipeId = '577d1a8e3dcc7d0c76cb72d1';
 			// performRecipeBookDELETERecipe(userId, unknownRecipeId, {responseType: 'full'})
 			// .then(function(response) {
@@ -249,7 +265,7 @@ describe('the Recipe Image endpoints', function() {
 			done();
 		});
 
-		it('attempting to DELETE an image for a recipe that does not exist will return 404', function(done) {
+		it('attempting to DELETE an image for a recipe that does not exist will return 404', function (done) {
 			done();
 		});
 	});
