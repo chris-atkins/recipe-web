449cf155b47c05e100c4e0e7a3a50612c26e0ac0
Chris <chrisatkins55@gmail.com>
2025-01-12 19:51:59 -0500

    Update dependencies to get karma-headless working

--- karma-headless.conf.js
+++ karma-headless.conf.js
@@ -1,7 +1,9 @@
+"use strict";
+
 module.exports = function (config) {
 	var baseProjectConfig = require('./karma.conf.js');
 
-	baseProjectConfig(config)
+	baseProjectConfig(config);
 
 	config.set({
 
